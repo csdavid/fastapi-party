6c093987c7028d056d748b2a06351a73af7fcf52
David Cruz <david.cruz@anam.gob.mx>
2025-07-23 11:28:50 -0600

    ch-16 infinite scroll

--- party_app/routes/party_list.py
+++ party_app/routes/party_list.py
@@ -1,24 +1,46 @@
 from datetime import date
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Query
 from fastapi.responses import HTMLResponse
-from sqlmodel import Session, select
+from sqlmodel import Session, select, func
 
 from party_app.dependency import Templates, get_session
 from party_app.models import Party
 
 router = APIRouter(prefix="", tags=["parties"])
 
+_PAGE_SIZE = 6
+
 
 @router.get("/", name="party_list_page", response_class=HTMLResponse)
 def party_list_page(
-    request: Request, templates: Templates, session: Session = Depends(get_session)
+    request: Request,
+    templates: Templates,
+    session: Session = Depends(get_session),
+    page: int = Query(1, ge=1),
 ):
     today = date.today()
-    parties = session.exec(select(Party).where(Party.party_date >= today)).all()
+
+    num_all_parties = session.exec(
+        select(func.count(Party.uuid)).where(Party.party_date >= today)
+    ).one()
+    offset = (page - 1) * _PAGE_SIZE
+
+    parties = session.exec(
+        select(Party).where(Party.party_date >= today).offset(offset).limit(_PAGE_SIZE)
+    ).all()
+
+    next_page = page + 1 if (offset + _PAGE_SIZE) <= num_all_parties else None
+
+    htmx_request = request.headers.get("HX-Request", None)
+
+    if htmx_request:
+        template_name = "party_list/partial_party_list.html"
+    else:
+        template_name = "party_list/page_party_list.html"
 
     return templates.TemplateResponse(
         request=request,
-        name="party_list/page_party_list.html",
-        context={"parties": parties},
+        name=template_name,
+        context={"parties": parties, "next_page": next_page},
     )
--- party_app/tests/test_party_list.py
+++ party_app/tests/test_party_list.py
@@ -31,3 +31,42 @@ def test_party_list_page_returns_list_of_future_parties(
     assert response.status_code == status.HTTP_200_OK
     assert len(response.context["parties"]) == 1
     assert response.context["parties"] == [valid_party]
+
+
+def test_party_list_page_returns_paginated_response(
+    session: Session,
+    client: TestClient,
+    create_party: Callable[..., Party],
+):
+    for i in range(11):
+        create_party(
+            session=session,
+            venue=f"Venue {i + 1}",
+        )
+
+    url = app.url_path_for("party_list_page")
+
+    response_first_page = client.get(url)
+    assert response_first_page.status_code == status.HTTP_200_OK
+    assert len(response_first_page.context["parties"]) == 6
+
+    response_second_page = client.get(f"{url}?page=2")
+    assert response_second_page.status_code == status.HTTP_200_OK
+    assert len(response_second_page.context["parties"]) == 5
+
+
+def test_party_list_page_returns_correct_template(
+    session: Session, client: TestClient, create_party: Callable[..., Party]
+):
+    create_party(session=session)
+
+    url = app.url_path_for("party_list_page")
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.template.name == "party_list/page_party_list.html"
+
+    htmx_response = client.get(url, headers={"HX-Request": "true"})
+    assert htmx_response.status_code == status.HTTP_200_OK
+    assert htmx_response.template.name == "party_list/partial_party_list.html"
